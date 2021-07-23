7abee9390a8911b9661750bb3b3d76ad526a214b
Marcelo Morais <marcelomorais002@gmail.com>
2021-07-23 17:39:49 -0300

    comitando

---
+++ AluraFlix/script.js
@@ -0,0 +1,20 @@
+
+const figure = document.createElement('figcaption');
+const img = document.createElement('img');
+const title = document.createElement('figcaption');
+const listBanners = document.querySelector('.list-banners');
+
+figure.classList.add('wrapper-banner');
+
+img.src = 'imagens/VingadoresUltimato.jpg';
+img.alt = 'banner of the movie';
+img.classList.add('main-banner');
+img.style.width='70px';
+
+title.textContent = 'Vingadores Ultimato';
+title.classList.add('main-caption');
+
+figure.insertAdjacentElement('beforeend',img);
+figure.insertAdjacentElement('beforeend',title);
+
+listBanners.insertAdjacentElement('beforeend',figure);
